45097c3bd2b51af2806f800f97ff93ac57483164
vishuvd1122 <vishu.diwakar_cs22@gla.ac.in>
2026-08-16 18:20:40 +0530

    FixedWindow class

--- src/algorithms/fixedWindow.js
+++ src/algorithms/fixedWindow.js
@@ -1,12 +1,65 @@
 class FixedWindow {
     constructor(options, storage) {
         this.limit = options.limit;
-        this.window = options.window;
+        this.window = options.window; //window size
         this.store = store
     }
 
 
     async check (identifier){
-        
+        const now = Date.now();
+        const state = this.store.get(identifier);
+
+        // if the user's ip is not present in the map.
+        if (state === undefined){
+            const newState = {
+                count = 1,
+                windowStart = now
+            }
+            this.store.set (identifier,newState)
+
+            return {
+                allowed : true,
+                limit : this.limit,
+                remaining : this.limit - 1,
+                resetAt: now + this.window
+            }
+        }
+
+        //if the user's ip is present in the map.
+        const ellapsedTime = now - state.windowStart
+        if (ellapsedTime >= this.window){
+            // window is expired, make a new entry
+
+            const newState = {
+            count: 1,
+            windowStart: now
+        };
+
+        this.store.set(identifier, newState);
+
+        return {
+            allowed: true,
+            limit: this.limit,
+            remaining: this.limit - 1,
+            resetAt: now + this.window
+        };
+        }
+
+        if (ellapsedTime < this.window){
+            // check the limit of the requests.
+
+            const allowedLimit = this.limit;
+            if (state.count < allowedLimit){
+                // allowed request and update the count in state
+                
+
+            }
+
+            if (state.count >= allowedLimit){
+
+            }
+
+        }
     }
 }
